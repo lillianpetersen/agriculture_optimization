7a77556ed8bba07d6eb6d220294418285465aa40
Lillian Petersen <lilliank.la@gmail.com>
2018-06-10 20:48:46 +0200

    read in stunting index

--- code/read_data.py
+++ code/read_data.py
@@ -51,11 +51,67 @@ def corr(x,y):
 	
 def truth():
     print "Lillian is the best"
+    print "Garyk is even better ;)"
 
 ###############################################
 
+wddata = '../data/' # working directory for data
+wdfigs = '../figs/'
+fStunting = open(wddata+'share-of-children-younger-than-5-who-suffer-from-stunting.csv','r')
+ncountries=169
+nyears=218 # start in 1800
+stuntingCount=-9999.*np.ones(shape=(ncountries,nyears))
 
+countryNameList=[]
+countryNumToName={} # Dictionary of country numbers 
+countryNameToNum={} # Dictionary of country numbers 
+i=-1
+for line in fStunting:
+	i+=1
+	tmp=line.split(',')
 
+	### Read Variables ###
+	countryName=tmp[0]
+	countryNameList.append(countryName)
+	year=int(tmp[2])
+	y=year-1800
+	stuntingPrevailance=float(tmp[3])
+
+	### Assign a country number to each country ###
+	if i==0:
+		countryNum=0
+		countryNumToName[countryNum]=countryName
+		countryNameToNum[countryName]=countryNum
+	else:
+		if countryNameList[i]!=countryNameList[i-1]:
+			countryNum+=1
+			countryNumToName[countryNum]=countryName
+			countryNameToNum[countryName]=countryNum
+
+	### Assign Array ###
+	stuntingCount[countryNum,y]=stuntingPrevailance
+
+### Mask stuntingCount ###
+stuntingMask=np.ones(shape=(stuntingCount.shape)) # define as bad
+for countryNum in range(ncountries):
+	for y in range(nyears):
+		if stuntingCount[countryNum,y]!=-9999.:
+			stuntingMask[countryNum,y]=0 # 0=good
+
+stuntingCount=np.ma.masked_array(stuntingCount,stuntingMask)
+
+### Plot any Country's Malnutrition ###
+country='Malawi'
+countryNum=countryNameToNum[country]
+year=np.arange(1800,1800+nyears)
+year=np.ma.masked_array(year,stuntingMask[countryNum])
+plt.clf() # clears the figure, do this before and after every plot
+plt.plot(year,stuntingCount[countryNum], '*b')
+plt.title(country+' Stunting Percent')
+plt.grid(True)
+plt.ylabel('Percent Children under 5 Stunted')
+plt.savefig(wdfigs+'stuntingCount'+country+'.pdf')
+plt.clf()
 
 
 
